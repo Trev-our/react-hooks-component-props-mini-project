11be14ec64ca207ec3f3bb48e5caf194f518ef67
Trevour <trevour.ambia@student.moringaschool.com>
2025-04-25 12:16:48 +0300

    work done

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import blogData from "../data/blog";
+
+function App() {
+  const { name, image, about, posts } = blogData;
+
+  return (
+    <div className="App">
+      <Header name={name} />
+      <About image={image} about={about} />
+      <ArticleList posts={posts} />
+    </div>
+  );
+}
+
+export default App;
+
